0a7ebcdc2a00d35e83582297fa8a308a855d7ef1
Travis <travisfinch01@gmail.com>
2019-07-31 13:15:17 -0400

    Push past

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -256,10 +256,8 @@ bool GetKernelStakeModifier(uint256 hashBlockFrom, uint64_t& nStakeModifier, int
 
     // loop to find the stake modifier later by a selection interval
     while (nStakeModifierTime < pindexFrom->GetBlockTime() + nStakeModifierSelectionInterval) {
-        if (!pindexNext) {
-            // Should never happen
-            return error("Null pindexNext\n");
-        }
+        if (!pindexNext && nStakeModifier)
+            return true;
 
         pindex = pindexNext;
         pindexNext = chainActive[pindexNext->nHeight + 1];
